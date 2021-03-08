f7630867d85fc502cee5a27081d1fa9db3291cd5
ashishbinu <ashishbinu90@gmail.com>
2021-03-08 21:08:10 +0530

    Use correct method to parse url

--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -6,7 +6,7 @@ class RemoteServices {
 
   static Future<ClassRoom> fetchSubjects() async {
     const String _url = 'https://api.mocki.io/v1/58c6b541';
-    final response = await client.get(_url);
+    final response = await client.get(Uri.parse(_url));
     if (response.statusCode == 200) {
       return ClassRoom.fromRawJson(response.body);
     } else {
